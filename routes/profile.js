2071cabcf7582ab860443a28b73df96bf10697ab
Yosra Yousfi <yousfi.yosra@gmail.com>
2020-04-30 14:50:50 +0200

    add menu to api layout

--- routes/profile.js
+++ routes/profile.js
@@ -68,12 +68,13 @@ router.post("/edit", uploadCloud.single("photo"), (req, res, next) => {
   req.user.dob = req.body.dob;
   req.user.origin = req.body.origin;
 
-  req.user.save()
-    .then(user => {
+  req.user
+    .save()
+    .then((user) => {
       console.log(`Success ${user} was added to the database`);
       res.redirect("/profile/profile");
     })
-    .catch(error => {
+    .catch((error) => {
       console.log(error);
     });
 });
@@ -166,12 +167,16 @@ router.post("/search", (req, res, next) => {
 router.post("/:userID/follow", (req, res, next) => {
   const loggedInUser = req.user;
   const userIdToFollow = req.params.userID;
-  
-  if (!loggedInUser._id.equals(userIdToFollow) && !loggedInUser.following.includes(userIdToFollow)) {
+
+  if (
+    !loggedInUser._id.equals(userIdToFollow) &&
+    !loggedInUser.following.includes(userIdToFollow)
+  ) {
     loggedInUser.following.push(userIdToFollow);
   }
-  loggedInUser.save()
-    .then (user => {
+  loggedInUser
+    .save()
+    .then((user) => {
       res.redirect(`/profile/${userIdToFollow}`);
     })
     .catch((err) => {
@@ -182,15 +187,18 @@ router.post("/:userID/follow", (req, res, next) => {
 router.post("/posts/:postID/like", (req, res, next) => {
   const loggedInUser = req.user;
   const postIdToLike = req.params.postID;
-  
+
   Post.findById(postIdToLike)
     .then((post) => {
-      if (!loggedInUser._id.equals(post.owner) && !post.liked.includes(loggedInUser._id)) {
+      if (
+        !loggedInUser._id.equals(post.owner) &&
+        !post.liked.includes(loggedInUser._id)
+      ) {
         post.liked.push(loggedInUser._id);
       }
-      return post.save() //return: waiting for .save before moving to redirect
+      return post.save(); //return: waiting for .save before moving to redirect
     })
-    .then (post => {
+    .then((post) => {
       res.redirect(`/profile/posts/details/${postIdToLike}`);
     })
     .catch((err) => {
@@ -199,24 +207,24 @@ router.post("/posts/:postID/like", (req, res, next) => {
 });
 //POST share data from api to database
 router.post("/share", (req, res, next) => {
-    Post.create({
-      image: req.body.image,
-      description: req.body.description,
-      owner: req.user._id,
+  Post.create({
+    image: req.body.image,
+    description: req.body.description,
+    owner: req.user._id,
+  })
+    .then((post) => {
+      console.log(post);
+      res.redirect("/profile/profile");
     })
-      .then((post) => {
-        console.log(post);
-        res.redirect("/profile/profile");
-      })
-      .catch((err) => {
-        next(err);
-      });
-}); 
+    .catch((err) => {
+      next(err);
+    });
+});
 //your own profile
 router.get("/profile", (req, res, next) => {
-  console.log('following!!', req.user.following)
-  User.populate(req.user, {path: "following"}).then (populatedUser => {
-    console.log('populated following!!', populatedUser.following)
+  console.log("following!!", req.user.following);
+  User.populate(req.user, { path: "following" }).then((populatedUser) => {
+    console.log("populated following!!", populatedUser.following);
   });
 
   Post.find({ owner: req.user._id })
@@ -224,7 +232,7 @@ router.get("/profile", (req, res, next) => {
     .sort({ created_at: -1 })
     .populate({ path: "owner" })
     .then((posts) => {
-      posts.forEach(post => {
+      posts.forEach((post) => {
         // replace spaces in URL
         post.image = encodeURI(post.image);
       });
